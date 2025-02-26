bfeacbf1fcb5ea88388f8a21a9081152dea32830
Azizulakbar89 <azizulakbar89@gmail.com>
2025-02-26 19:22:05 +0700

    bismillah ya allah

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -39,7 +39,7 @@ class BlogController extends Controller
         if ($request->file('image')) {
             $extension = $request->file('image')->getClientOriginalExtension();
             $newName = $request->judul . '-' . now()->timestamp . '.' . $extension;
-            $request->file('image')->storeAs('gambar/' . $newName);
+            $request->file('image')->storeAs($newName);
         }
 
         // Create the article
